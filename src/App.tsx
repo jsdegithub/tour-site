a0644210d141b791909a936b1ff81fe7e216d3c0
jsdegithub <def_myself@qq.com>
2021-07-25 20:02:50 +0800

    路由系统搭建

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,18 @@
 import React from "react";
 import styles from "./App.module.css";
-import {
-  Header,
-  Footer,
-  SideMenu,
-  Carousel,
-  ProductCollection,
-  BusinessPartners,
-} from "./components";
-import { Row, Col, Typography } from "antd";
-import { productList1, productList2, productList3 } from "./mockups";
-import sideImage from "./assets/images/sider_2019_12-09.png";
-import sideImage2 from "./assets/images/sider_2019_02-04.png";
-import sideImage3 from "./assets/images/sider_2019_02-04-2.png";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HomePage } from "./pages";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <div className={styles["page-content"]}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              国内游推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              爆款推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
-        <BusinessPartners />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/signIn" render={() => <h2>登录页面</h2>} />
+          <Route render={() => <h2>404 页面不存在</h2>} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
