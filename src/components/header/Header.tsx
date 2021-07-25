17a4ccbab1dac8881b06aab5d2059742046a145b
jsdegithub <def_myself@qq.com>
2021-07-26 01:55:48 +0800

    使用useXXX钩子让组件可以使用RouteComponentProps

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,8 +3,19 @@ import { Layout, Typography, Input, Dropdown, Menu, Button } from "antd";
 import { GlobalOutlined } from "@ant-design/icons";
 import logo from "../../assets/logo.svg";
 import styles from "./Header.module.css";
+import { useHistory, useLocation, useParams, useRouteMatch } from "react-router-dom";
 
 export const Header: React.FC = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const params = useParams();
+  const routeMatch = useRouteMatch();
+
+  console.log("history", history);
+  console.log("location", location);
+  console.log("params", params);
+  console.log("routeMatch", routeMatch);
+
   return (
     <div className={styles["app-header"]}>
       <div className={styles["top-header"]}>
@@ -23,16 +34,18 @@ export const Header: React.FC = () => {
             语言
           </Dropdown.Button>
           <Button.Group className={styles["button-group"]}>
-            <Button>注册</Button>
-            <Button>登录</Button>
+            <Button onClick={() => history.push("/register")}>注册</Button>
+            <Button onClick={() => history.push("/signIn")}>登录</Button>
           </Button.Group>
         </div>
       </div>
       <Layout.Header className={styles["main-header"]}>
-        <img src={logo} alt="logo" className={styles["App-logo"]} />
-        <Typography.Title level={3} className={styles.title}>
-          React旅游网
-        </Typography.Title>
+        <span onClick={() => history.push("/")} className={styles["App-logo-wrapper"]}>
+          <img src={logo} alt="logo" className={styles["App-logo"]} />
+          <Typography.Title level={3} className={styles.title}>
+            React旅游网
+          </Typography.Title>
+        </span>
         <Input.Search
           className={styles["search-input"]}
           placeholder={"请输入旅游目的地、主题、或关键字"}
