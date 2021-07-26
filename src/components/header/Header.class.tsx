bf130c6e2a4595dbc8d273d0a3c59c971324d72e
jsdegithub <def_myself@qq.com>
2021-07-26 20:11:20 +0800

    添加新语言

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -20,16 +20,30 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
     store.subscribe(() => {
       // 这句不能不写
       const storeState = store.getState();
-      this.setState({ language: storeState.language });
+      this.setState({
+        language: storeState.language,
+        languageList: storeState.languageList,
+      });
     });
   }
 
   menuClickHandler = (e) => {
-    const action = {
-      type: "change_language",
-      payload: e.key,
-    };
-    store.dispatch(action);
+    if (e.key === "new") {
+      const action = {
+        type: "add_language",
+        payload: {
+          code: "new_lang",
+          name: "新语言",
+        },
+      };
+      store.dispatch(action);
+    } else {
+      const action = {
+        type: "change_language",
+        payload: e.key,
+      };
+      store.dispatch(action);
+    }
   };
 
   render() {
@@ -46,6 +60,7 @@ class HeaderComponent extends React.Component<RouteComponentProps, State> {
                   {this.state.languageList.map((i) => (
                     <Menu.Item key={i.code}>{i.name}</Menu.Item>
                   ))}
+                  <Menu.Item key={"new"}>添加新语言</Menu.Item>
                 </Menu>
               }
               icon={<GlobalOutlined />}
