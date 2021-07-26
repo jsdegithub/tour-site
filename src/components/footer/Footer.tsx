cccc9d4abb72fd58b06775c5bf5e21a4eda2dab4
jsdegithub <def_myself@qq.com>
2021-07-27 03:43:20 +0800

    在languageReducer中调用i18n.changeLanguage实现语言切换

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Layout, Typography } from "antd";
+import { useTranslation } from "react-i18next";
 
 export const Footer: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <Layout.Footer style={{ textAlign: "center" }}>
-      <Typography.Title level={3}>版权所有 @ React 旅游网</Typography.Title>
+      <Typography.Title level={3}>{t("footer.detail")}</Typography.Title>
     </Layout.Footer>
   );
 };
