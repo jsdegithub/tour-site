5566c02899508c69f1f328354fa1828f6bb47dac
jsdegithub <def_myself@qq.com>
2021-07-30 19:34:20 +0800

    extraReducers

--- src/redux/productDetail/slice.ts
+++ src/redux/productDetail/slice.ts
@@ -16,15 +16,10 @@ const initialState: ProductDetailState = {
 export const getProductDetail = createAsyncThunk(
   "productDetail/getProductDetail",
   async (touristRouteId: string, thunkAPI) => {
-    thunkAPI.dispatch(productDetailSlice.actions.fetchStart());
-    try {
-      const { data } = await axios.get(
-        `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
-      );
-      thunkAPI.dispatch(productDetailSlice.actions.fetchSuccess(data));
-    } catch (error) {
-      thunkAPI.dispatch(productDetailSlice.actions.fetchFail(error.message));
-    }
+    const { data } = await axios.get(
+      `http://123.56.149.216:8080/api/touristRoutes/${touristRouteId}`
+    );
+    return data;
   }
 );
 
@@ -41,6 +36,20 @@ export const productDetailSlice = createSlice({
       state.error = null;
     },
     fetchFail: (state, action: PayloadAction<string | null>) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+  extraReducers: {
+    [getProductDetail.pending.type]: (state) => {
+      state.loading = true;
+    },
+    [getProductDetail.fulfilled.type]: (state, action) => {
+      state.data = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    [getProductDetail.rejected.type]: (state, action: PayloadAction<string | null>) => {
       state.error = action.payload;
       state.loading = false;
     },
